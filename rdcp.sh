7a569fad0160a8371bd5b35ed4193305a8e58531
ShengZixuan <chengzixuan21a@mails.ucas.ac.cn>
2024-09-13 08:38:10 +0000

    modify rdcp.sh

--- rdcp.sh
+++ rdcp.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-limit=100
+limit=300000
 
 i=0
+plim=5000
 while [ $i -lt $limit ]
 do
     ((i++))
-    cat rdcp.c >> rdcptest
+    if [ "$i" -eq "$plim" ] 
+    then
+        cat rdcp.c >> rdcptest
+        echo -e "plim: $plim\n"
+        ((plim+=5000))
+    fi
 done
